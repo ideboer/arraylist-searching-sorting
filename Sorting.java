1f9e7e289507d5505775c4b633765796aea1ad41
ideboer <isabella@songdeboer.net>
2020-04-12 16:56:42 -0600

    finished bubbleSort, not done with insertionSort

--- Sorting.java
+++ Sorting.java
@@ -23,19 +23,54 @@ public class Sorting {
     }
 
     public static void insertionSort(ArrayList<Integer> list) {
-        int temp;
-        for(int i = 0; i < list.size(); i++) {
-            for(int j = i + 1; j > 0; j++) {
-                if (j < i) {
-                    temp = list.get(i);
-                    list.set(i, j);
-                    list.set(j, temp);
+        int temp;  
+        int elementsOn = 2;
+        boolean inOrder = false;
+
+        while (elementsOn != list.size())
+        {
+            while (!inOrder)
+            {
+                inOrder = true;
+                for (int i = 0; i < elementsOn; i++)
+                {
+                    if (list.get(i) > list.get(i + 1))
+                    {
+                        temp = list.get(i);
+                        list.set(i, i + 1);
+                        list.set(i + 1, temp);
+                        inOrder = false;
+                    }
                 }
+            }
+            elementsOn++;
+            inOrder = false;           
+        }
+    }  
+    
+
+    public static void bubbleSort(ArrayList<Integer> list) {
+        int temp;
+        int swaps;
+        boolean done = false;
+        while(!done) {
+            swaps = 0;
+            for(int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i + 1) < list.get(i)) {
+                    temp = list.get(i);
+                    list.set(i, list.get(i+1));
+                    list.set(i+1, temp);
+                    swaps++;
+                }
+            }
+            if (swaps == 0) {
+                done = true;
             }
         }
     }
 
     public static void main(String[] args) {
+        //selectionSort tests
         /*ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
         ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(3, 10, 7, 9, 1, 4, 5, 8, 6, 2));
@@ -48,16 +83,30 @@ public class Sorting {
         System.out.println(list2);
         System.out.println(list3);*/
 
-        ArrayList<Integer> list4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        //insertionSort tests
+        /*ArrayList<Integer> list4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         ArrayList<Integer> list5 = new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
         ArrayList<Integer> list6 = new ArrayList<Integer>(Arrays.asList(3, 10, 7, 9, 1, 4, 5, 8, 6, 2));
 
-        selectionSort(list4);
-        selectionSort(list5);
-        selectionSort(list6);
+        insertionSort(list4);
+        insertionSort(list5);
+        insertionSort(list6);
         
         System.out.println(list4);
         System.out.println(list5);
-        System.out.println(list6);
+        System.out.println(list6);*/
+
+        //bubbleSort tests
+        ArrayList<Integer> list7 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        ArrayList<Integer> list8 = new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+        ArrayList<Integer> list9 = new ArrayList<Integer>(Arrays.asList(3, 10, 7, 9, 1, 4, 5, 8, 6, 2));
+
+        bubbleSort(list7);
+        bubbleSort(list8);
+        bubbleSort(list9);
+        
+        System.out.println(list7);
+        System.out.println(list8);
+        System.out.println(list9);
     }
 }
