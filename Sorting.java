55119774750da21e67f43b8da14993c6465c781a
ideboer <isabella@songdeboer.net>
2020-04-12 14:56:44 -0600

    finished insertionSort

--- Sorting.java
+++ Sorting.java
@@ -22,8 +22,21 @@ public class Sorting {
         }
     }
 
+    public static void insertionSort(ArrayList<Integer> list) {
+        int temp;
+        for(int i = 0; i < list.size(); i++) {
+            for(int j = i + 1; j > 0; j++) {
+                if (j < i) {
+                    temp = list.get(i);
+                    list.set(i, j);
+                    list.set(j, temp);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        /*ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
         ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(3, 10, 7, 9, 1, 4, 5, 8, 6, 2));
 
@@ -33,6 +46,18 @@ public class Sorting {
         
         System.out.println(list1);
         System.out.println(list2);
-        System.out.println(list3);
+        System.out.println(list3);*/
+
+        ArrayList<Integer> list4 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        ArrayList<Integer> list5 = new ArrayList<Integer>(Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+        ArrayList<Integer> list6 = new ArrayList<Integer>(Arrays.asList(3, 10, 7, 9, 1, 4, 5, 8, 6, 2));
+
+        selectionSort(list4);
+        selectionSort(list5);
+        selectionSort(list6);
+        
+        System.out.println(list4);
+        System.out.println(list5);
+        System.out.println(list6);
     }
 }
